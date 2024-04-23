9ece3c7323240f13239913c3a91a0bda7b2fe2df
ZeroDay0utplay <hama.tm662@gmail.com>
2024-04-23 19:49:36 +0100

    fix login service

--- back-end/services/UserServices/login.service.js
+++ back-end/services/UserServices/login.service.js
@@ -4,18 +4,20 @@ const bcrypt = require("bcryptjs");
 
 async function login(email, password, pool){
     try {
-        const user = await getData(pool, "email", email, "users");
-        const user_id = user[0].user_id;
-        if (user.length > 0){
-            const userPassword = user[0].password;
-            const isMatch = bcrypt.compareSync(password, userPassword);
-            if (!isMatch) {
-                return ["Invalid password", user_id];
-            }
-            if (!(user.is_verified)) return ["Verify your account", user_id];
-            return ["user logged in successfully", user_id];
+        const users = await getData(pool, "email", email, "users");
+        if (users.length === 0){
+            return ["user not found", user_id];
         }
-        return ["user not found", user_id];
+        const user = users[0];
+        const user_id = user.user_id;
+        const userPassword = user.password;
+        const isMatch = bcrypt.compareSync(password, userPassword);
+        if (!isMatch) {
+            return ["Invalid password", user_id];
+        }
+        if (!(user.is_verified)) return ["Verify your account", user_id];
+        return ["user logged in successfully", user_id];
+        
     } catch (error) {
         return "Login ERROR";
     }
